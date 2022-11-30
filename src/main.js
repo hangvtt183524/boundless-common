9b0c9c9fe2fa5f29ccf3b1dc6255eafd580044f4
hangvu <nanh.trang.140@gmail.com>
2022-11-30 23:45:31 +0700

    add config to main

--- src/main.js
+++ src/main.js
@@ -2,24 +2,22 @@ import Vue from 'vue';
 import singleSpaVue from 'single-spa-vue';
 
 import App from './App.vue';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
+Vue.config.ignoredElements = ['trix-editor']
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import './bootstrap'
 
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
     render(h) {
       return h(App, {
-        props: {
-          // single-spa props are available on the "this" object. Forward them to your component as needed.
-          // https://single-spa.js.org/docs/building-applications#lifecycle-props
-          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
-          /*
-          name: this.name,
-          mountParcel: this.mountParcel,
-          singleSpa: this.singleSpa,
-          */
-        },
+        props: {},
       });
     },
   },
