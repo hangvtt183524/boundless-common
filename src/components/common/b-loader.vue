56709c419d5a9e5bc75af17d055b1a3aab920fa4
hangvtt183524 <nanh.trang.140@gmail.com>
2022-12-04 01:57:49 +0700

    create b-loader component

---
+++ src/components/common/b-loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout align-center fill-height justify-center>
+    <v-progress-circular
+        :color="color"
+        :size="size"
+        indeterminate
+        data-test-loader
+    />
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 50
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
